ecbac4c10fe99ab6947d44abdd32b9b3b6275dab
Anamar12! <benchea.ana.95@gmail.com>
2021-04-15 22:03:03 +0100

    Added the icon from API

--- src/app.js
+++ src/app.js
@@ -27,12 +27,16 @@ function displayTemperature(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed);
     let dateElement = document.querySelector("#date");
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute ("alt", response.data.weather[0].description);
+
 
 }   
 
 let apiKey = "8a6d72f8c89473f99e31152caddb4270";
-let city = "London";
+let city = "London";    
 let apiUrl = 
 `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature); 
